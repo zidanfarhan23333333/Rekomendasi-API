7ea62fcec5d91c48c0b013df62b76ff11e9f6fe7
unknown <sfarkhan48@gmail.com>
2026-06-28 18:30:38 +0700

    update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,27 +1,16 @@
 "use strict";
 
-require("dotenv").config();
-
 const { PrismaClient } = require("@prisma/client");
 const { PrismaPg } = require("@prisma/adapter-pg");
 const { Pool } = require("pg");
-
-if (!process.env.DATABASE_URL) {
-  console.error("DATABASE_URL tidak ditemukan di .env");
-}
-
-console.log("DB URL", process.env.DATABASE_URL);
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
 const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
-const prisma = global.prisma ?? new PrismaClient({ adapter });
-
-if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma;
-}
+console.log("DB URL", process.env.DATABASE_URL);
 
 module.exports = prisma;
